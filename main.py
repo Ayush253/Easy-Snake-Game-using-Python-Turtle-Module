ce4c694981df47386b7523be13631ca4713bfaba
Ayush V. Patel <81207867+Ayush253@users.noreply.github.com>
2022-11-08 12:55:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -34,12 +34,14 @@ while game_is_on:
         scoreboard.increase_score()
 
     if snake.head.xcor() > 265 or snake.head.xcor() < -265 or snake.head.ycor() > 265 or snake.head.ycor() < -265:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset_score()
+        snake.reset_game()
 
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset_score()
+            snake.reset_game()
 
 screen.exitonclick()
